9f6769e433157b2d22d173b2f5f11569b11587cb
bheo1st <19200551.bsi.ac.id>
2022-05-24 19:07:08 +0700

    add report

--- application/application/modules/rpt/views/virtual/v_virtual.php
+++ application/application/modules/rpt/views/virtual/v_virtual.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-body">
-        <h4 class="card-title">Laporan Rack Server</h4>
+        <h4 class="card-title">Laporan Virtual Server</h4>
         <div class="row">
             <div class="col-12">
                 <div>
